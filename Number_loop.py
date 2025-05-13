4b747ad3ff1260ab1e34a5948d444e2ed10062be
PardhivAryan <114898680+PardhivAryan@users.noreply.github.com>
2025-05-13 10:57:18 +0530

    Add files via upload

---
+++ Number_loop.py
@@ -0,0 +1,24 @@
+numbers = []
+
+for i in range(1, 11):
+    numbers.append(i)
+#    Let's go through the loop iteration-by-iteration:
+# 1st Iteration (i = 1)
+# numbers.append(1) → numbers = [1]
+# print(numbers) → Output: [1]
+ 
+# 2nd Iteration (i = 2)
+# numbers.append(2) → numbers = [1, 2] 
+# print(numbers) → Output: [1, 2]
+
+# 3rd Iteration (i = 3)
+# numbers.append(3) → numbers = [1, 2, 3]
+# print(numbers) → Output: [1, 2, 3]
+
+# 4th Iteration (i = 4)
+# numbers.append(4) → numbers = [1, 2, 3, 4]
+
+
+
+  
+    print(numbers)
