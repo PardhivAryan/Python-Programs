7578ff2b35851dd665bde4444143396a56b92166
PardhivAryan <114898680+PardhivAryan@users.noreply.github.com>
2025-12-12 15:14:14 +0530

    Add files via upload

--- PrimeNumber.py
+++ PrimeNumber.py
@@ -1,12 +1,9 @@
 def prime(n):
     if n <= 1:
-        print('not defined')
         return False
     elif n == 2:
-        print('Prime Number')
         return True
     elif n % 2 == 0:
-        print('Composite Number')
         return False
     
     for i in range(3, int(n ** 0.5)+ 1, 2):
@@ -14,13 +11,16 @@ def prime(n):
         #which means(start,stop,step) if we take 29 it is divisible 3 and 5. 6 means it exceeds limit 
         #it dont divide by four because the step is 2 so it jumps two numbers in range(3,6,2) which means 29 not 
         #divided by 3 and 5
-        if n%i==0: # means it checks 29 % with 3 and 5 == 0
+        if n % i == 0: # means it checks 29 % with 3 and 5 == 0
             print('not prime')
             return False
     return True
     
 n = int(input("enter number: "))
-if prime(n):
+result = prime(n)
+if result is True:
     print(f"{n} is Prime number")
-else:
+elif result is False:
     print(f"{n} is Composite number")
+else:
+    print("0 and 1 are neither Prime nor Composite")
